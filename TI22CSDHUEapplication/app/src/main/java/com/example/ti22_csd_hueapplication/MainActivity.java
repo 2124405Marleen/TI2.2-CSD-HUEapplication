fb02bd8f878ced9c1bbe4d8cd624209d4ef1917a
Marleen <mroubos2@avans.nl>
2019-11-14 16:24:46 +0100

    Project add

---
+++ TI22CSDHUEapplication/app/src/main/java/com/example/ti22_csd_hueapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ti22_csd_hueapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
